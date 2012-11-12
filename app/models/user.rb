5df7146bfffc9f195b5aab4a5c9feb47d25758fe
Kevin Ryan Tao <kevin.ryan.tao@gmail.com>
2012-11-12 10:26:24 -0600

    Added password support for the user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :phone_number, :skype_id
+  attr_accessible :email, :name, :phone_number, :skype_id, :password, :password_confirmation
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112000734) do
+ActiveRecord::Schema.define(:version => 20121112024647) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "skype_id"
     t.string   "phone_number"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
 
 end
