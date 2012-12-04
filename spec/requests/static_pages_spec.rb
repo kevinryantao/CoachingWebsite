46ec8fd54f2433a10be072804c9508c31bd18534
Kevin Ryan Tao <kevin.ryan.tao@gmail.com>
2012-12-04 13:00:50 -0600

    Added full_title helper method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "LucidLivingCoaching" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -9,10 +11,15 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "LucidLivingCoaching | Home")
+                                :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -26,7 +33,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                                :text => "LucidLivingCoaching | Help")
+                                :text => "#{base_title} | Help")
     end
   end
 
@@ -40,7 +47,7 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                                :text => "LucidLivingCoaching | About Us")
+                                :text => "#{base_title} | About Us")
     end
   end
 
@@ -54,7 +61,7 @@ describe "Static pages" do
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                                :text => "LucidLivingCoaching | Contact")
+                                :text => "#{base_title} | Contact")
     end
   end
 end
